9d99e35b355a416671713fda51010c67ec1d6acf
Tasneem <tasneemgazal5@gmail.com>
2024-03-16 09:00:19 +0200

    handle signIn

--- src/components/sign in/Sign_in.jsx
+++ src/components/sign in/Sign_in.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import InputCom from "../shared/InputCom";
-import { Box, Button, Container, FormGroup, Grid, Paper, Typography, useMediaQuery } from "@mui/material";
+import { Box, Button, Container, Grid, Paper, Typography, useMediaQuery } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { purple } from "@mui/material/colors";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import axios from "axios";
 
 const ColorButton = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(purple[500]),
@@ -16,6 +18,69 @@ const ColorButton = styled(Button)(({ theme }) => ({
 
 export default function Sign_in() {
   const matches = useMediaQuery((theme) => theme.breakpoints.up('md'));
+  let navigate = useNavigate();
+  let[userToken,setUserToken] = useState();
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const onSubmit = async (users) => {
+    try {
+      const {data} = await axios.post("http://localhost:3000/api/v1/grad/auth/signIn", users);
+      console.log(data);
+      if (data.message === "valid account") {
+        localStorage.setItem("userToken", data.token);
+        setUserToken(data.token);
+        if(data.role === "admin") {
+        navigate("/dashboard");
+        
+        }
+      }
+    } catch (error) {
+      console.log("Error occurred:", error)
+    }
+  
+  
+  }
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validateOnBlur: true,
+    validateOnChange: false,
+  });
+
+  const inputs = [
+    {
+      id: "email",
+      type: "email",
+      name: "email",
+      title: "User Email",
+      value: formik.values.email,
+    },
+    {
+      id: "password",
+      type: "password",
+      name: "password",
+      title: "User Password",
+      value: formik.values.password,
+    },
+  ];
+
+  const renderInputs = inputs.map((input, index) => (
+    <InputCom
+      type={input.type}
+      name={input.name}
+      id={input.id}
+      title={input.title}
+      value={input.value}
+      key={index}
+      placeholder={input.name}
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      touched={formik.touched}
+    />
+  ));
 
   return (
     <Box
@@ -24,7 +89,6 @@ export default function Sign_in() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        
       }}
     >
       <Container maxWidth="lg">
@@ -37,30 +101,29 @@ export default function Sign_in() {
                 backgroundColor: "#fff",
                 p: 3
               }}
-            > 
-             <Box sx={{textAlign:"center"}}>
-              <img src="image/ptuk.jpg" alt="ptuk logo"  width="40px" height="40px"/>
-              <Typography variant="p" sx={{display:"block", pb:1}}>Palestine Technical University</Typography>
-             <Typography variant="h4" sx={{ fontSize:"30px",textAlign: "center", mb: 4, fontWeight: "bold" }}>
-                Sign In
-              </Typography>
-             </Box>
-              <form sx={{ width: "100%", mb: 3 }}>
-                <InputCom placeholder={"Email"} type={"email"} />
-                <InputCom placeholder={"Password"} type={"password"}/>
+            >
+              <Box sx={{ textAlign: "center" }}>
+                <img src="image/ptuk.jpg" alt="Palestine Technical University logo" width="40px" height="40px" />
+                <Typography variant="body2" sx={{ display: "block", pb: 1 }}>Palestine Technical University</Typography>
+                <Typography variant="h4" sx={{ fontSize: "30px", textAlign: "center", mb: 4, fontWeight: "bold" }}>
+                  Sign In
+                </Typography>
+              </Box>
+              <form onSubmit={formik.handleSubmit} sx={{ width: "100%", mb: 3 }}>
+                {renderInputs}
                 <Link style={{ textDecoration: "none" }} to='/sendCode'>
                   <Typography variant="body2" sx={{ textAlign: "right", color: "primary", cursor: "pointer" }}>
                     Forgot Password?
                   </Typography>
                 </Link>
-                <ColorButton fullWidth>Sign in</ColorButton>
+                <ColorButton fullWidth type="submit">Sign in</ColorButton>
               </form>
             </Paper>
           </Grid>
           {matches && (
             <Grid item xs={12} md={6}>
               <Box sx={{ display: "flex", justifyContent: "center" }}>
-                <img src="image/boy on graduation.gif" alt="" style={{ maxWidth: "100%", height: "auto" }} />
+                <img src="image/boy on graduation.gif" alt="Boy on Graduation" style={{ maxWidth: "100%", height: "auto" }} />
               </Box>
             </Grid>
           )}
@@ -69,3 +132,4 @@ export default function Sign_in() {
     </Box>
   );
 }
+
