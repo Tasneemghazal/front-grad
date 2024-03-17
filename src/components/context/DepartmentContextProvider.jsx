9166b0b2820c209642abcf02222210b7de7500fc
Tasneem <tasneemgazal5@gmail.com>
2024-03-17 21:46:45 +0200

    add removeDep to department context

--- src/components/context/DepartmentContextProvider.jsx
+++ src/components/context/DepartmentContextProvider.jsx
@@ -13,10 +13,26 @@ export default function DepartmentContextProvider({children}) {
             console.log(err);
           }
     }
+    const removeDep = async (depId) => {
+      try {
+        const token = localStorage.getItem("userToken");
+        const { data } = await axios.delete(
+          `${import.meta.env.VITE_API_URL}/admin/deleteDep/${depId}`,
+          { headers: { token: `Bearer ${token}` } }
+        );
+        console.log(data);
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+        
+        
   return (
     <DepartmentContext.Provider
       value={{
-        getDepartments
+        getDepartments,
+        removeDep
       }}
     >
       {children}
