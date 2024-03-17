b323cb870bdff571741a23d6d0d33e253bff9cd5
Tasneem <tasneemgazal5@gmail.com>
2024-03-17 08:50:19 +0200

    create user inputs file

---
+++ src/components/dashboard/create/userInputFields.js
@@ -0,0 +1,52 @@
+import { useFormik } from 'formik';
+
+const userInputFields = (initialValues, onSubmit, handleFieldChange) => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validateOnBlur: true,
+    validateOnChange: false,
+  });
+
+  const inputs = [
+    {
+      id: "name",
+      type: "text",
+      name: "name",
+      title: "Name",
+      value: formik.values.name,
+    },
+    {
+      id: "email",
+      type: "email",
+      name: "email",
+      title: "Email",
+      value: formik.values.email,
+    },
+    {
+      id: "password",
+      type: "password",
+      name: "password",
+      title: "Password",
+      value: formik.values.password,
+    },
+    {
+      id: "phoneNumber",
+      type: "text",
+      name: "phoneNumber",
+      title: "PhoneNumber",
+      value: formik.values.phoneNumber,
+    },
+    {
+      id: "img",
+      type: "file",
+      name: "img",
+      title: "Image",
+      onChange: handleFieldChange,
+    },
+  ];
+
+  return { formik, inputs };
+};
+
+export default userInputFields;
