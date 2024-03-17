e92947b6bd3962313902f2d4ad2a325b5ff3cb12
Tasneem <tasneemgazal5@gmail.com>
2024-03-17 08:49:03 +0200

    add DepartmentContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './layouts/Routes.jsx'
+import DepartmentContextProvider from './components/context/DepartmentContextProvider.jsx'
 
 export default function App() {
   return (
-    <>
+    <DepartmentContextProvider>
     <RouterProvider router={router}/>
-    </>
+    </DepartmentContextProvider>
   )
 }
