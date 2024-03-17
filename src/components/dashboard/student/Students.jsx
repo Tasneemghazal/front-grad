c7a7c0613c3854c530aa6ae627ecff1d4b3f24eb
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-17 20:33:41 +0200

    delete crud import

--- src/components/dashboard/student/Students.jsx
+++ src/components/dashboard/student/Students.jsx
@@ -1,6 +1,5 @@
 import { Box } from '@mui/material'
 import React from 'react'
-import Crud from '../../shared/Crud'
 import CustomTable from '../../shared/CustomTable.jsx'
 
 export default function Students() {
