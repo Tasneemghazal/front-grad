59a202cc2d8ff9eb8c852eee24f3a304176d54ce
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-18 15:52:12 +0200

    edit the color of header of the table

--- src/components/shared/CustomTable.jsx
+++ src/components/shared/CustomTable.jsx
@@ -9,18 +9,22 @@ import TableRow from '@mui/material/TableRow';
 import { Paper, IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
+const RedTableHead = styled(TableHead)({
+  backgroundColor: 'rgba(45, 3, 62, 0.4)', // Set the background color to red
+});
+
 export default function CustomTable({ columns, data, onDelete }) {
   return (
     <TableContainer component={Paper} sx={{ border: "1px solid rgba(43, 1, 62, 0.5)" }}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
+        <RedTableHead>
           <TableRow>
             {columns && columns.map((column, index) => (
               <TableCell key={index} align="center">{column}</TableCell>
             ))}
             <TableCell align="center">Actions</TableCell>
           </TableRow>
-        </TableHead>
+        </RedTableHead>
         <TableBody>
           {data && data.map((row, rowIndex) => (
             <TableRow key={rowIndex}>
