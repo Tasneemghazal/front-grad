2d887f1d5fdd4c5d1870ed23d083f936ad0d7108
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-18 15:45:48 +0200

    Add user context provider and student context provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './layouts/Routes.jsx'
 import DepartmentContextProvider from './components/context/DepartmentContextProvider.jsx'
+import StudentContextProvider from './components/context/StudentContextProvider.jsx'
+import UserContextProvider from './components/context/UserContextProvider.jsx'
 
 export default function App() {
   return (
+    <UserContextProvider>
+    <StudentContextProvider>
     <DepartmentContextProvider>
     <RouterProvider router={router}/>
     </DepartmentContextProvider>
+    </StudentContextProvider>
+    </UserContextProvider>
   )
 }
