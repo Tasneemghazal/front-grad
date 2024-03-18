e3d4925a854e07b0f38b71ae3c0d45e6921d2740
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-18 19:19:47 +0200

    display projects in table

--- src/components/dashboard/projects/ViewProject.jsx
+++ src/components/dashboard/projects/ViewProject.jsx
@@ -1,11 +1,53 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import CustomTable from '../../shared/CustomTable.jsx'
+import { Box, Typography } from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import CustomTable from '../../shared/CustomTable';
+import { ProjectContext } from '../../context/ProjectContextProvider.jsx';
 
 export default function ViewProject() {
+  const { getProjects, removeProject } = useContext(ProjectContext); 
+  const [tableData, setTableData] = useState([]);
+  const [tableColumns, setTableColumns] = useState([]);
+
+  const removeMyProject = async (proId) => {
+    const res = await removeProject(proId);
+    console.log(res);
+    if (res.message === "success") {
+      setTableData(tableData.filter((pro) => pro._id !== proId));
+    }
+    return res;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getProjects();
+        if (res.projects.length > 0) {
+          const projectKey = Object.keys(res.projects[0]);
+          const columns = ['_id', 'name', 'group','supervisorName','depId'];
+          setTableColumns(columns);
+          setTableData(res.projects);
+        }
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+    fetchData();
+  }, [getProjects]);
+
   return (
-    <Box sx={{ width: { md: '60%' }, ml: { md: "300px" }, mt: 10, mx: { xs: 4 } }}>
-    <CustomTable/>
+    <Box sx={{ width: { md: '60%' }, ml: { md: "400px" }, mt: 5, mx: { xs: 4 } }}>
+      <Typography
+        variant="h3"
+        sx={{ textAlign: "center", fontSize: "30px", my: 5, fontWeight: "bold" }}
+      >
+        Show all projects
+      </Typography>
+      
+      <CustomTable
+        columns={tableColumns}
+        data={tableData}
+        onDelete={removeMyProject}
+      />
     </Box>
-  )
+  );
 }
