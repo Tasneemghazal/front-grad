8d0c3d2b686d9ab8710947a9a89b1772784ebde6
Tasneem <tasneemgazal5@gmail.com>
2024-03-19 06:48:28 +0200

    make cardCom dynamic

--- src/components/shared/CardCom.jsx
+++ src/components/shared/CardCom.jsx
@@ -1,29 +1,26 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
+import { Box, CardContent, Link, Typography } from "@mui/material";
+import LinkIcon from "@mui/icons-material/Link";
 
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-
-import { red } from "@mui/material/colors";
-
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-
-export default function CardComp({ image }) {
+export default function CardComp({ image, name, supervisorName, thesis, group }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="194"
-        image={image} 
-        alt="Paella dish"
-      />
-      <CardHeader
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
+      <CardMedia component="img" height="194" image={image} alt="Paella dish" />
+      <CardContent>
+        <Typography>
+          {name}
+        </Typography>
+        <Typography>
+          {supervisorName}
+        </Typography>
+        <Typography>{group}</Typography>
+        <Link href={thesis} sx={{color:"rgba(43, 1, 62, 1)"}}>
+          <LinkIcon />
+        </Link>
+      </CardContent>
     </Card>
   );
 }
