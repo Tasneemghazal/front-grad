04c48973c25d6c03c60d54a03e4c5dcfcf2b1a06
Tasneem <tasneemgazal5@gmail.com>
2024-03-19 06:48:03 +0200

    handle get project by depId

--- src/components/mainPage/projects/Project.jsx
+++ src/components/mainPage/projects/Project.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   Box,
   Container,
@@ -10,30 +10,51 @@ import {
 } from "@mui/material";
 import CardCom from "../../shared/CardCom.jsx";
 import style from "../../shared/shared.module.css";
+import { DepartmentContext } from "../../context/DepartmentContextProvider.jsx";
+import { ProjectContext } from "../../context/ProjectContextProvider.jsx";
+
 export default function Project() {
-  const [activeIndex, setActiveIndex] = useState(0); // Default active index
+  const [activeIndex, setActiveIndex] = useState(0); // Default active index set to 0
   const [open, setOpen] = useState(false); // Dropdown open state
+  const [departments, setDepartments] = useState([]);
+  const [selectedDepartmentProjects, setSelectedDepartmentProjects] = useState([]);
 
-  const majors = [
-    "All Projects",
-    "Computer Systems Engineering",
-    "Electrical Engineering",
-    "Energy Engineering",
-    "Mechanical Engineering",
-    "Electrical Eng-Industrial Automation",
-    "Communications Engineering Technology",
-    "Building Engineering",
-    "Sustainable Energy Engineering",
-    "Civil Engineering and Sustainable Structures/Co-operative",
-    "Automotive Engineering/Co-operative",
-  ];
+  const { getDepartments } = useContext(DepartmentContext);
+  const { getDepProject } = useContext(ProjectContext);
 
-  // Check if screen size is less than medium
+  async function fetchData() {
+    try {
+      const res = await getDepartments();
+      if (res.deps.length > 0) {
+        setDepartments(res.deps);
+        // Set the default selected department projects
+        await getProjectsForDepartment(res.deps[0]._id);
+      }
+    } catch (error) {
+      console.error("Error fetching departments:", error);
+    }
+  }
+
+  const getProjectsForDepartment = async (depId) => {
+    try {
+      const projects = await getDepProject(depId);
+      setSelectedDepartmentProjects(projects.projects);
+    } catch (error) {
+      console.error("Error fetching projects for department:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("md"));
 
-  const handleListItemClick = (index) => {
+  const handleListItemClick = async (index) => {
     setActiveIndex(index);
     setOpen(false); // Close the dropdown when an item is clicked
+    const depId = departments[index]._id;
+    await getProjectsForDepartment(depId);
   };
 
   return (
@@ -41,70 +62,94 @@ export default function Project() {
       sx={{
         backgroundColor: "#fff",
         py: { xs: 2, md: 3 },
-        my:3,
-        borderTop:"1px dashed  rgba(43, 1, 62, 0.4)",
+        my: 3,
+        borderTop: "1px dashed  rgba(43, 1, 62, 0.4)",
       }}
     >
       <Container>
-        <Box sx={{ pb:1, width: "50%", margin: "auto", textAlign: "center", position:"relative" }}>
-          <Typography variant="h2" sx={{ fontSize: { xs: "24px", sm: "30px", md: "36px" }, fontWeight: "900", mb:4 }}>
+        <Box
+          sx={{
+            pb: 1,
+            width: "50%",
+            margin: "auto",
+            textAlign: "center",
+            position: "relative",
+          }}
+        >
+          <Typography
+            variant="h2"
+            sx={{
+              fontSize: { xs: "24px", sm: "30px", md: "36px" },
+              fontWeight: "900",
+              mb: 4,
+            }}
+          >
             Explore Our College To See Previous Projects
           </Typography>
-          <Box sx={{width:"100%"}} className={`${style.border}`}></Box>
+          <Box sx={{ width: "100%" }} className={`${style.border}`} />
         </Box>
-        <Grid container spacing={2} sx={{pt:5}}>
-          {isSmallScreen ? ( 
+        <Grid container spacing={2} sx={{ pt: 5 }}>
+          {isSmallScreen ? (
             <Grid item xs={12}>
               <Select
                 open={open}
                 onClose={() => setOpen(false)}
                 onOpen={() => setOpen(true)}
-                value={majors[activeIndex]}
-                onChange={(e) => handleListItemClick(majors.indexOf(e.target.value))}
+                value={departments[activeIndex]}
+                onChange={(e) =>
+                  handleListItemClick(departments.indexOf(e.target.value))
+                }
                 fullWidth
               >
-                {majors.map((major, index) => (
+                {departments.map((major, index) => (
                   <MenuItem key={index} value={major}>
-                    {major}
+                    {major.name}
                   </MenuItem>
                 ))}
               </Select>
             </Grid>
           ) : (
-           
             <Grid item xs={12} md={4}>
-              {majors.map((major, index) => (
+              {departments.map((major, index) => (
                 <Box
                   key={index}
                   sx={{
-                    backgroundColor: activeIndex === index ? " rgba(43, 1, 62, 0.7)" : "inherit",
+                    backgroundColor:
+                      activeIndex === index
+                        ? "rgba(43, 1, 62, 0.7)"
+                        : "inherit",
                     color: activeIndex === index ? "#fff" : "inherit",
                     cursor: "pointer",
                     fontSize: { xs: "16px", sm: "20px", md: "23px" },
                     mb: 1,
                     "&:hover": {
-                      backgroundColor: " rgba(43, 1, 62, 0.4)",
+                      backgroundColor: "rgba(43, 1, 62, 0.4)",
                       color: "#fff",
-                      borderTopRightRadius:30,
-                      borderBottomRightRadius:30,                    },
+                      borderTopRightRadius: 30,
+                      borderBottomRightRadius: 30,
+                    },
                     ...(activeIndex === index && {
-                      borderTopRightRadius:30,
-                      borderBottomRightRadius:30,                    }),
+                      borderTopRightRadius: 30,
+                      borderBottomRightRadius: 30,
+                    }),
                   }}
                   onClick={() => handleListItemClick(index)}
                 >
-                  <Typography variant="body1" sx={{py:1, ml:1}}>{major}</Typography>
+                  <Typography variant="body1" sx={{ py: 1, ml: 1 }}>
+                    {major.name}
+                  </Typography>
                 </Box>
               ))}
             </Grid>
           )}
           <Grid item xs={12} md={8}>
             <Grid container spacing={2}>
-              {Array.from({ length: 6 }).map((_, index) => (
-                <Grid item xs={12} sm={6} md={4} key={index}>
-                  <CardCom image="image/cover.jpg"/>
-                </Grid>
-              ))}
+              {Array.isArray(selectedDepartmentProjects) &&
+                selectedDepartmentProjects.map((project, index) => (
+                  <Grid item xs={12} sm={6} md={6} key={index}>
+                    <CardCom image={project.img} name={project.name}  supervisorName={project.supervisorName} thesis={project.thesis} group={project.group}/>
+                  </Grid>
+                ))}
             </Grid>
           </Grid>
         </Grid>
