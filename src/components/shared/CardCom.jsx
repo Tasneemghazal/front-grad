1cb3d0bfea8cccf1469a9ffd7e4cdf89bc3392af
Tasneem <tasneemgazal5@gmail.com>
2024-03-19 17:12:27 +0200

    slice project name and group

--- src/components/shared/CardCom.jsx
+++ src/components/shared/CardCom.jsx
@@ -4,13 +4,16 @@ import CardMedia from "@mui/material/CardMedia";
 import { Avatar, Box, CardContent, Link, Typography } from "@mui/material";
 
 export default function CardComp({ image, name, supervisorName, thesis, group }) {
+  
+  const truncatedName = name.length > 15 ? name.slice(0, 15) + "..." : name;
+  const studentName = group.length>1 ? group[0]+", ..." :group;
   return (
     <Card sx={{ maxWidth: 345, border: "1px solid rgba(43, 1, 62, 0.4)" }}>
       <CardMedia component="img" height="194" image={image} alt="Paella dish" />
       <CardContent>
         <Typography sx={{ display: "flex" }}>
           <Typography sx={{ fontWeight: "bold", mr: 1 }}>Project Name:</Typography>
-          {name}
+          {truncatedName}
         </Typography>
         <Typography sx={{ display: "flex" }}>
           <Typography sx={{ fontWeight: "bold", mr: 1 }}>Super. Name:</Typography>
@@ -18,7 +21,7 @@ export default function CardComp({ image, name, supervisorName, thesis, group })
         </Typography>
         <Typography sx={{ display: "flex" }}>
           <Typography sx={{ fontWeight: "bold", mr: 1 }}>Group Name:</Typography>
-          {group}
+          {studentName}
         </Typography>
         <Box sx={{ display: 'flex', justifyContent: 'center',mt:1 }}>
           <Link href={thesis}>
