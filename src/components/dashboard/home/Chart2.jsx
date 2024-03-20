df176d20a658b988a08589f8fecdd859b3ca912c
Tasneem <tasneemgazal5@gmail.com>
2024-03-20 21:53:59 +0200

    handle users number

--- src/components/dashboard/home/Chart2.jsx
+++ src/components/dashboard/home/Chart2.jsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
-
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContextProvider.jsx'; 
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { userContext } from '../../context/StudentContextProvider.jsx';
 const data = [
   { value: 7, label: 'HOD' },
   { value: 10, label: 'Supervisor' },
@@ -14,6 +18,34 @@ const size = {
 };
 
 export default function Chart2() {
+  const { getUsers} = useContext(UserContext); 
+  const { userData} = useContext(userContext); 
+  const [headCount, setHeadCount] = useState();
+  const [superCount, setSuperCount]  = useState();
+  const data = [
+    { value: headCount, label: 'HOD' },
+    { value: superCount, label: 'Supervisor' },
+    { value: userData, label: 'Student' },
+  
+  ];
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getUsers();
+        if (res.users.length > 0) {
+          
+          const filteredHOD = res.users.filter(user => user.role === "headOfDepartment");
+          setHeadCount(filteredHOD.length);
+          const filteredSuper = res.users.filter(user => user.role === "supervisor");
+          setSuperCount(filteredSuper.length);
+        }
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <PieChart
       series={[
