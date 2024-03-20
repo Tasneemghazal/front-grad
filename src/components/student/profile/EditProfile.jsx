00100c298386b473356e96591d0145a7965ee801
Tasneem <tasneemgazal5@gmail.com>
2024-03-20 11:40:25 +0200

    handle edit proile

--- src/components/student/profile/EditProfile.jsx
+++ src/components/student/profile/EditProfile.jsx
@@ -1,20 +1,93 @@
-import React, { useRef } from "react";
-import { Avatar, Box, Button, Container, Grid, Typography } from "@mui/material";
+import React, { useContext, useState, useEffect } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 import WavingHandIcon from "@mui/icons-material/WavingHand";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import { UserContext } from "../../context/StudentContextProvider.jsx";
 import InputCom from "../../shared/InputCom.jsx";
-
+import { useFormik } from "formik";
+import axios from "axios";
 export default function EditProfile() {
-  const fileInputRef = useRef(null);
+  const { extractNameFromToken } = useContext(UserContext);
+  const token = localStorage.getItem("userToken");
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const onSubmit = async (users) => {
+    try {
+      const { data } = await axios.patch(
+        "http://localhost:3000/api/v1/grad/supervisor/editProfile",
+        users, {
+          headers: { token: `Bearer ${token}` }}
+      );
+      console.log(data);
+    } catch (error) {
+      console.log("Error occurred:", error);
+    }
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const name = extractNameFromToken();
+        setUserName(name.name);
+        setUserEmail(name.email);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  const initialValues = {
+    phoneNumber: phoneNumber,
+    password: "",
+  };
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validateOnBlur: true,
+    validateOnChange: false,
+  });
+  const inputs = [
+    {
+      id: "phoneNumber",
+      type: "text",
+      name: "phoneNumber",
+      title: "User phoneNumber",
+      value: formik.values.phoneNumber,
+    },
+    {
+      id: "password",
+      type: "password",
+      name: "password",
+      title: "User Password",
+      value: formik.values.password,
+    },
+  ];
+
+  const renderInputs = inputs.map((input, index) => (
+    <InputCom
+      type={input.type}
+      name={input.name}
+      id={input.id}
+      title={input.title}
+      value={input.value}
+      key={index}
+      placeholder={input.name}
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      touched={formik.touched}
+    />
+  ));
 
   const handleUploadClick = () => {
-    fileInputRef.current.click();
-  };
-
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    // Handle the file upload logic here
-    console.log("Selected file:", file);
+    // Define functionality to handle profile image upload
+    console.log("Upload clicked");
   };
 
   return (
@@ -24,14 +97,21 @@ export default function EditProfile() {
         position: "relative",
         top: -115,
         backgroundColor: "rgba(43, 1, 62, 0.2)",
-        
       }}
     >
-      <Container sx={{ display: "flex", height: "100%", alignItems: "center", justifyContent: {xs:"center",md:"start"} ,pt: { xs: 5, md: 6 } }}>
+      <Container
+        sx={{
+          display: "flex",
+          height: "100%",
+          alignItems: "center",
+          justifyContent: { xs: "center", md: "start" },
+          pt: { xs: 5, md: 6 },
+        }}
+      >
         <Typography
           sx={{ fontSize: { xs: "30px", md: "40px" }, fontWeight: "bold" }}
         >
-          Hello Fatima!
+          Hello {userName}!
           <WavingHandIcon
             sx={{
               fontSize: { xs: "30px", md: "40px" },
@@ -44,7 +124,16 @@ export default function EditProfile() {
       <Container>
         <Grid container spacing={2}>
           {/* Profile Picture Section */}
-          <Grid item xs={12} md={4} sx={{ borderRight: { xs: 0, md: "1px solid #eee" }, height: { xs: "auto", md: "400px" }, mt: 5 }}>
+          <Grid
+            item
+            xs={12}
+            md={4}
+            sx={{
+              borderRight: { xs: 0, md: "1px solid #eee" },
+              height: { xs: "auto", md: "400px" },
+              mt: 5,
+            }}
+          >
             <Box
               sx={{
                 display: "flex",
@@ -53,9 +142,9 @@ export default function EditProfile() {
                 flexDirection: "column",
               }}
             >
-              <Box sx={{ position: "relative",pt:{xs:0,md:8} }}>
+              <Box sx={{ position: "relative", pt: { xs: 0, md: 8 } }}>
                 <Avatar
-                  alt="Fatima's Profile"
+                  alt={`${userName}'s Profile`}
                   src="/image/toto.jpeg"
                   sx={{
                     width: "130px",
@@ -84,30 +173,58 @@ export default function EditProfile() {
                 </Box>
               </Box>
               <Typography sx={{ mt: 5 }}>Change your profile image!</Typography>
-              <input
-                type="file"
-                ref={fileInputRef}
-                style={{ display: "none" }}
-                onChange={handleFileChange}
-              />
             </Box>
           </Grid>
 
           {/* Edit Info Section */}
-          <Grid item xs={12} md={8} sx={{ height: { xs: "auto", md: "400px" }, mt: 5 }}>
-            <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%" }}>
+          <Grid
+            item
+            xs={12}
+            md={8}
+            sx={{ height: { xs: "auto", md: "400px" }, mt: 5 }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                height: "100%",
+              }}
+            >
               <Box>
                 <Typography variant="h6" sx={{ fontWeight: "bold", mb: 2 }}>
                   Edit Your Info
                 </Typography>
                 <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-                  <InputCom placeholder={"Name"} type={"text"} />
-                  <InputCom placeholder={"Email"} type={"text"} />
-                  <InputCom placeholder={"PhoneNumber"} type={"text"} />
-                  <InputCom placeholder={"Password"} type={"password"} />
-                </Box>
-                <Box>
-                  <Button sx={{ bgcolor: "green", color: "#fff", "&:hover": {bgcolor: "green",color: "#000"} }}>Save</Button>
+                  <form onSubmit={formik.handleSubmit}>
+                    <InputCom
+                      placeholder={"Name"}
+                      type={"text"}
+                      name={"name"}
+                      value={userName}
+                      disabled={true}
+                    />
+                    <InputCom
+                      placeholder={"Email"}
+                      type={"email"}
+                      name={"email"}
+                      value={userEmail}
+                      disabled={true}
+                    />
+                    {renderInputs}
+                    <Button
+                      sx={{
+                        bgcolor: "green",
+                        color: "#fff",
+                        "&:hover": { bgcolor: "green", color: "#000" },
+                      }}
+                      type="submit"
+                    >
+                      Save
+                    </Button>
+                  </form>
+                  <Box>
+                  </Box>
                 </Box>
               </Box>
             </Box>
