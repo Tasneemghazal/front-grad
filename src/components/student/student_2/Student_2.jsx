69d211c45e202c8bab0940539fe1d0ea4c668920
Leen Odeh <leenodeh287@gmail.com>
2024-03-22 07:08:31 +0300

    Separate Chat Component

--- src/components/student/student_2/Student_2.jsx
+++ src/components/student/student_2/Student_2.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import HailIcon from '@mui/icons-material/Hail';
 import PeopleIcon from '@mui/icons-material/People';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
@@ -8,6 +7,7 @@ import { Container } from '@mui/material'
 import StudentTab1 from './StudentTab1.jsx';
 import StudentTab2 from './StudentTab2.jsx';
 import StudentTab3 from './StudentTab3.jsx';
+import ChatBotHelper from './ChatBotHelper';
 
 export default function Student_2() {
   return (
@@ -17,7 +17,7 @@ export default function Student_2() {
       { label: <HailIcon/>, content: <StudentTab1/> },
       { label: <PeopleIcon/>, content: <StudentTab2/> },
       { label: <BorderColorIcon/>, content: <StudentTab3/> },
-      { label: <SmartToyIcon/>, content: 'Content for Item Four' }
+      { label: <SmartToyIcon/>, content: <ChatBotHelper/> }
     ]}
   />
    </Container>
