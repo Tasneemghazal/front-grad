bb521a74db5f446061a74b7281a46a12fd2b2903
Leen Odeh <leenodeh287@gmail.com>
2024-03-22 06:22:18 +0300

    UseNavigate to route page booking

--- src/components/student/Booking/Booking.jsx
+++ src/components/student/Booking/Booking.jsx
@@ -1,13 +1,20 @@
 import "./Booking.css";
 import { Box, Container } from "@mui/system";
+import { useNavigate } from "react-router-dom";
 
 const Booking = () => {
+  const navigate = useNavigate();
+
+  const handleBookingClick = () => {
+    navigate("/section-booking");
+  };
   return (
     <Container>
-      <Box className="marquee">
+      <Box className="marquee" onClick={handleBookingClick}>
         <span style={{ color: "red" }}>
           Booking Is Available.&nbsp;&nbsp;&nbsp;
-        </span>  <span style={{ color: "red" }}>
+        </span>{" "}
+        <span style={{ color: "red" }}>
           Booking Is Available.&nbsp;&nbsp;&nbsp;
         </span>
       </Box>
