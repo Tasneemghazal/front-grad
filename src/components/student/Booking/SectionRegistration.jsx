ffd80147f9028b423213cab10ac365cec7b91b4f
Tasneem <tasneemgazal5@gmail.com>
2024-03-23 19:08:09 +0200

    import required components

--- src/components/student/Booking/SectionRegistration.jsx
+++ src/components/student/Booking/SectionRegistration.jsx
@@ -1,34 +1,21 @@
-import EventIcon from "@mui/icons-material/Event";
-import { useState, useEffect, useContext } from "react";
-import {
-  Container,
-  Typography,
-  Box,
-  Grid,
-  Paper,
-  Button,
-} from "@mui/material";
+import React, { useState, useEffect, useContext } from "react";
+import { Container, Typography, Box, Grid, Paper, Button } from "@mui/material";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import axios from "axios";
-import InputCom from "../../shared/InputCom.jsx";
-import { useFormik } from "formik";
-import { userContext } from "../../context/StudentContextProvider.jsx";
+import SupervisorName from "./SupervisorName.jsx";
+import SectionForm from "./SectionForm.jsx";
 import { SectionContext } from "../../context/SectionContextProvider.jsx";
-import { UserContext } from "../../context/UserContextProvider.jsx";
-
+import EventIcon from "@mui/icons-material/Event";
 const SectionRegistration = () => {
   const token = localStorage.getItem("userToken");
   const [section, setSection] = useState([]);
   const { getSections } = useContext(SectionContext);
-  const { extractDepIdFromToken } = useContext(UserContext);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const sections = await getSections();
-        const depSections = sections.filter(sec => sec.depId === extractDepIdFromToken())
-        setSection(depSections);
+        setSection(sections);
       } catch (error) {
         console.error("Error fetching sections:", error);
       }
@@ -38,7 +25,11 @@ const SectionRegistration = () => {
   }, [getSections]);
 
   const handleSectionUpdate = (updatedSection) => {
-    setSection(prevSections => prevSections.map(sec => sec._id === updatedSection._id ? updatedSection : sec));
+    setSection(prevSections =>
+      prevSections.map(sec =>
+        sec._id === updatedSection._id ? updatedSection : sec
+      )
+    );
   };
 
   return (
@@ -102,110 +93,6 @@ const SectionRegistration = () => {
         ))}
       </Grid>
     </Container>
-  );
-};
-
-const SupervisorName = ({ userId }) => {
-  const { getUserById } = useContext(UserContext);
-  const [supervisorName, setSupervisorName] = useState("");
-
-  useEffect(() => {
-    const fetchSupervisorName = async () => {
-      try {
-        const name = await getUserById(userId);
-        setSupervisorName(name.user.name);
-      } catch (error) {
-        console.error("Error fetching supervisor name:", error);
-      }
-    };
-
-    fetchSupervisorName();
-  }, [getUserById, userId]);
-
-  return (
-    <Typography sx={{ fontStyle: "italic" }}>
-      Supervisor: {supervisorName}
-    </Typography>
-  );
-};
-
-const SectionForm = ({ section, token, onUpdateSection }) => {
-  const { extractNameFromToken } = useContext(userContext);
-  const [studentId, setStudentId] = useState("");
-  const initialValues = { text: "" };
-
-  useEffect(() => {
-    const fetchStudentId = async () => {
-      try {
-        const name = extractNameFromToken();
-        setStudentId(name._id);
-      } catch (error) {
-        console.error("Error fetching student id:", error);
-      }
-    };
-
-    fetchStudentId();
-  }, [extractNameFromToken]);
-
-  const onSubmit = async (values) => {
-    try {
-      const data = {
-        text: values.text,
-        studentId: studentId,
-        sectionId: section._id,
-      };
-
-      const response = await axios.post(
-        "http://localhost:3000/api/v1/grad/student/bookSection",
-        data,
-        { headers: { token: `Bearer ${token}` } }
-      );
-      console.log(response)
-
-      if (response.data.message === "success") {
-        // Update the visibility of the booked section to false
-        onUpdateSection({ ...section, visible: false });
-        console.log("Booking successful");
-      } else {
-        console.log("Booking failed");
-      }
-    } catch (error) {
-      console.log("Error occurred:", error);
-    }
-  };
-
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validateOnBlur: true,
-    validateOnChange: false,
-  });
-
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <InputCom
-        type="text"
-        placeholder="Enter your info"
-        name="text"
-        onChange={formik.handleChange}
-        value={formik.values.text}
-      />
-      <Box sx={{ textAlign: "center" }}>
-        <Button
-          type="submit"
-          sx={{
-            border: "1px solid rgba(43, 1, 62, 0.4)",
-            color: "white",
-            backgroundColor: "rgba(43, 1, 62, 0.7)",
-            '&:hover': {
-              backgroundColor: "rgba(43, 1, 62, 0.9)",
-            },
-          }}
-        >
-          Book
-        </Button>
-      </Box>
-    </form>
   );
 };
 
