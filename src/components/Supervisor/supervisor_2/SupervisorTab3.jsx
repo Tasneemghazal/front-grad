7ae44abff430b21f24dcf2fb643e5a882c8f2d67
Tasneem <tasneemgazal5@gmail.com>
2024-03-23 21:47:42 +0200

    handle getRequests

--- src/components/Supervisor/supervisor_2/SupervisorTab3.jsx
+++ src/components/Supervisor/supervisor_2/SupervisorTab3.jsx
@@ -1,27 +1,43 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { RequestContext } from '../../context/RequestContextProvider.jsx'
-import { Paper } from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import { RequestContext } from '../../context/RequestContextProvider.jsx';
+import CustomTable from '../../shared/CustomTable.jsx';
+import axios from 'axios';
 
 export default function SupervisorTab3() {
     const { getRequests } = useContext(RequestContext);
-    const [requests, setRequests] = useState([]);
-
+    const [tableData, setTableData] = useState([]);
+    const [tableColumns, setTableColumns] = useState([]);
+    const [rejRequest, setRejRequest] = useState([]); // Initialize as an object
+    const reject = async () => {
+        const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/supervisor/reject`, rejRequest);
+        console.log(data);
+    }
     useEffect(() => {
         const fetchData = async () => {
-            const { requests } = await getRequests();
-            console.log(requests);
-            setRequests(requests);
+            try {
+                const { requests } = await getRequests();
+                if (requests.length > 0) {
+                    const reqKeys = Object.keys(requests[0]);
+                    const columns = ["sectionId", "studentId", "text"];
+                    setTableColumns(columns);
+                    setTableData(requests);
+                    const requestData = requests.map(request => ({
+                        sectionId: request.sectionId,
+                        requestId: request._id
+                    }));
+                    
+                    setRejRequest(requestData[0]);
+                
+                }
+            } catch (e) {
+                console.log(e);
+            }
         }
         fetchData();
     }, []);
-
     return (
         <>
-            {requests.map((req) => (
-                <Paper key={req._id}>
-                    {req.text}
-                </Paper>
-            ))}
+            <CustomTable data={tableData} columns={tableColumns} request={false} onDelete={reject}/>
         </>
     );
 }
