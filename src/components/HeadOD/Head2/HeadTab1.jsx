4613707b5f4b70049fac7bf3c659b8a00a189809
Tasneem <tasneemgazal5@gmail.com>
2024-03-24 21:47:49 +0200

    handle get head sections

--- src/components/HeadOD/Head2/HeadTab1.jsx
+++ src/components/HeadOD/Head2/HeadTab1.jsx
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Box, Grid, Typography } from '@mui/material';
 import CardComp2 from '../../shared/CardComp2.jsx';
 import SpringModal from '../../shared/SpringModal.jsx';
+import { SectionContext } from '../../context/SectionContextProvider.jsx'
+import { UserContext } from '../../context/UserContextProvider.jsx';
+import SupervisorName from '../../student/Booking/SupervisorName.jsx';
+import GetStudentName from '../../shared/GetStudentName.jsx';
 
 export default function HeadTab1() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const token = localStorage.getItem("userToken");
+  const [section, setSection] = useState([]);
+  const { getSections } = useContext(SectionContext);
+  const { getUserById } = useContext(UserContext);
+  const [supervisorName, setSupervisorName] = useState("");
+  const [rowData, setRowData] = useState([]); 
 
-  const openModal = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const sections = await getSections();
+        setSection(sections);
+      } catch (error) {
+        console.error("Error fetching sections:", error);
+      }
+    };
+
+    fetchData();
+  }, [getSections]);
+
+  const openModal = (students) => {
+    setRowData(students)
     setIsModalOpen(true);
   };
 
@@ -35,18 +59,33 @@ export default function HeadTab1() {
         </Typography>
       </Box>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4}>
-          <CardComp2 title={"section 1"} description={"Description of section 1"} onClickLearnMore={openModal} onClickDelete={onClickDelete}/>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <CardComp2 title={"section 1"} description={"Description of section 1"} onClickLearnMore={openModal} onClickDelete={onClickDelete}/>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <CardComp2 title={"section 1"} description={"Description of section 1"} onClickLearnMore={openModal} onClickDelete={onClickDelete}/>
-        </Grid>
-       
+        {section.map((sec) =>
+          <Grid item xs={12} sm={6} md={4} key={sec._id}>
+            <CardComp2
+              title={`${sec.num}`}
+              // Call getUser asynchronously and set user data once resolved
+              description={
+                <>
+                  <SupervisorName userId={sec.userId} />
+                </>
+              }
+              onClickLearnMore={() => openModal(sec.students)} // Pass student IDs to openModal
+              onClickDelete={onClickDelete}
+            />
+          </Grid>
+        )}
       </Grid>
-      <SpringModal closeModal={closeModal} isModalOpen={isModalOpen} />
+      <SpringModal 
+        closeModal={closeModal} 
+        isModalOpen={isModalOpen}  
+        modalContent={
+          <>
+            {rowData.map(studentId => (
+              <GetStudentName key={studentId} userId={studentId} />
+            ))}
+          </>
+        }
+      />
     </Box>
   );
 }
