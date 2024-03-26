ef0b3bd1ca25dd9c834642c55ad05f1be1791ac4
Tasneem <tasneemgazal5@gmail.com>
2024-03-26 21:32:11 +0200

    handle adding task

--- src/components/Supervisor/supervisor_2/AddTask.jsx
+++ src/components/Supervisor/supervisor_2/AddTask.jsx
@@ -1,105 +1,180 @@
-import { Box, Button, Container, Grid, IconButton, Typography, useMediaQuery } from "@mui/material";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import React, { useState } from "react";
 import InputCom from "../../shared/InputCom.jsx";
 import BasicDateTimePicker from "../../shared/BasicDateTimePicker.jsx";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import UploadFile from "../../shared/UploadFile.jsx";
+import { useFormik } from "formik";
+import axios from "axios";
+import { SectionContext } from "../../context/SectionContextProvider.jsx";
+import SelectCom from "../../shared/SelectCom.jsx";
+import { useSnackbar } from "../../context/SnackbarProvider.jsx";
 
-export default function AddTask({title}) {
-    const [sectionNumber, setSectionNumber] = useState([""]);
-    const theme = useTheme();
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+export default function AddTask({ title }) {
+  const { getSuperSections } = useContext(SectionContext);
+  const token = localStorage.getItem("userToken");
+  const [sections, setSections] = useState([]);
+  const [selectedThesis, setSelectedThesis] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const theme = useTheme();
+  const { showSnackbar } = useSnackbar();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
-    const handleAddSection = () => {
-        setSectionNumber([...sectionNumber, ""]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const sec = await getSuperSections();
+      setSections(sec);
+      console.log(sections);
     };
-  
-    const handleRemoveSection = () => {
-      if (sectionNumber.length > 1) {
-        setSectionNumber(sectionNumber.slice(0, -1));
+    fetchData();
+  }, []);
+  const initialValues = {
+    txt: "",
+    file: "",
+    startDate: "",
+    endDate: "",
+  };
+
+  const onSubmit = async (values) => {
+    const formData = new FormData();
+    formData.append('txt', values.txt);
+    formData.append('task', selectedThesis);
+    formData.append('startDate', startDate);
+    formData.append('endDate', endDate);
+    sections.forEach((section, index) => {
+      if (values.sections.includes(section._id)) {
+        formData.append(`sections[${index}]`, section._id);
       }
-    };
+    });
   
-    const handleSectionNumberChange = (index, newNumber) => {
-      const newSectionNumber = [...sectionNumber];
-      newSectionNumber[index] = newNumber;
-      setSectionNumber(newSectionNumber);
-    };
+    try {
+      const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/supervisor/assignTask`, formData, {
+        headers: { token: `Bearer ${token}` },
+      });
+      console.log(data)
+      if (data.message === 'success') {
+        showSnackbar({ message: 'task added successfully', severity: 'success' });
+      }
+    } catch (error) {
+      console.error('Submission error:', error);
+    }
+  };
+  
 
-    return (
-        <Container>
-            <Box>
-                <Typography variant="h2" sx={{ fontWeight: "bold", pb: 2,fontSize:{xs:"40px",md:"60px"} }}>
-                  {title}
-                </Typography>
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validateOnBlur: true,
+    validateOnChange: false,
+  });
+
+  const inputs = [
+    {
+      id: "txt",
+      type: "text",
+      name: "txt",
+      title: "Text",
+      value: formik.values.txt,
+    },
+  ];
+  const renderInputs = inputs.map((input, index) => (
+    <InputCom
+      type={input.type}
+      name={input.name}
+      id={input.id}
+      title={input.title}
+      value={input.value}
+      placeholder={input.title}
+      onChange={input.onChange || formik.handleChange}
+      onBlur={formik.handleBlur}
+      touched={formik.touched}
+    />
+  ));
+  const handleSectionChange = (event) => {
+    formik.setFieldValue("sections", event.target.value);
+  };
+  return (
+    <Container>
+      <Box>
+        <Typography
+          variant="h2"
+          sx={{
+            fontWeight: "bold",
+            pb: 2,
+            fontSize: { xs: "40px", md: "60px" },
+          }}
+        >
+          {title}
+        </Typography>
+      </Box>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={isSmallScreen ? 12 : 7}>
+          <form onSubmit={formik.handleSubmit} encType="multipart/form-data">
+            {renderInputs}
+            <BasicDateTimePicker label="From" onChange={setStartDate} />
+            <BasicDateTimePicker label="To" onChange={setEndDate} />
+            <SelectCom
+              labelId="sections-label"
+              id="sections"
+              value={formik.values.sections || []} 
+              onChange={handleSectionChange}
+              label="sections"
+              options={sections.map((sec) => ({
+                value: sec._id,
+                label: sec.num,
+              }))}
+              multiple // Add the multiple prop here
+            />
+            <UploadFile
+              onFileChange={setSelectedThesis}
+              buttonText="Upload Thesis File"
+            />
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "rgba(43, 1, 62, 0.5)",
+                "&:hover": {
+                  backgroundColor: "rgba(43, 1, 62, 0.8)",
+                },
+                width: "100%",
+                mt: 1,
+              }}
+              type="submit"
+            >
+              Submit
+            </Button>
+          </form>
+        </Grid>
+
+        {!isSmallScreen && (
+          <Grid item xs={12} sm={5}>
+            <Box
+              sx={{
+                width: "90%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                pt: 3,
+              }}
+            >
+              <img
+                src="/image/addTask.png"
+                alt="Add Task"
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
             </Box>
-            <Grid container spacing={3}>
-                <Grid item xs={12} sm={isSmallScreen ? 12 : 7}>
-                    <form action="">
-                        <InputCom placeholder={"Title"} type={"text"} />
-                        <BasicDateTimePicker label={"Submission Date"} />
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <BasicDateTimePicker label={"From"} />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <BasicDateTimePicker label={"To"} />
-                            </Grid>
-                        </Grid>
-                        <InputCom placeholder={"Description"} type={"text"} />
-                        <Grid container spacing={2}>
-                            {sectionNumber.map((section, index) => (
-                                <Grid item xs={12} sm={6} key={index}>
-                                    <InputCom
-                                        placeholder={`Section ${index + 1}`}
-                                        type={"text"}
-                                        value={section}
-                                        onChange={(e) => handleSectionNumberChange(index, e.target.value)}
-                                    />
-                                </Grid>
-                            ))}
-                            <Grid item xs={12} sm={6}>
-                                <Grid container spacing={1}>
-                                    <Grid item xs={6} sm={6}>
-                                        <IconButton onClick={handleAddSection} size="large" sx={{ width: "40%", color: "black", "&:hover": { color: "rgba(43, 1, 62, 0.8)" } }}>
-                                            <AddCircleIcon sx={{ fontSize: 30 }} />
-                                        </IconButton>
-                                    </Grid>
-                                    <Grid item xs={6} sm={6}>
-                                        <IconButton onClick={handleRemoveSection} size="large" sx={{ width: "40%", color: "black", "&:hover": { color: "rgba(43, 1, 62, 0.8)" } }}>
-                                            <RemoveCircleIcon sx={{ fontSize: 30 }} />
-                                        </IconButton>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        <UploadFile />
-                        <Button
-                            variant="contained"
-                            sx={{
-                                backgroundColor: "rgba(43, 1, 62, 0.5)",
-                                "&:hover": {
-                                    backgroundColor: "rgba(43, 1, 62, 0.8)",
-                                },
-                                width: "100%",
-                                mt: 1
-                            }}
-                        >
-                            Submit
-                        </Button>
-                    </form>
-                </Grid>
-
-                {!isSmallScreen && (
-                    <Grid item xs={12} sm={5}>
-                        <Box sx={{ width: "90%", display: "flex", justifyContent: "center", alignItems: "center", pt: 3 }}>
-                            <img src="/image/addTask.png" alt="Add Task" style={{ maxWidth: "100%", height: "auto" }} />
-                        </Box>
-                    </Grid>
-                )}
-            </Grid>
-        </Container>
-    );
+          </Grid>
+        )}
+      </Grid>
+    </Container>
+  );
 }
