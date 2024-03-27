20ee9b8915e25f83c1572ed4f28d9a79d69fee04
Tasneem <tasneemgazal5@gmail.com>
2024-03-27 21:39:11 +0200

    handle task display

--- src/components/student/student_2/StudentTab3.jsx
+++ src/components/student/student_2/StudentTab3.jsx
@@ -1,19 +1,28 @@
-import { Box, Grid, Typography } from "@mui/material";
-import React from "react";
+import { Box, Grid} from "@mui/material";
+import React, { useContext, useEffect, useState } from "react";
 import BasicDateCalendar from "../../shared/BasicDateCalender.jsx";
-import TaskCard from "../../shared/TaskCard.jsx";
 import Task from "./Task.jsx";
 import Title from "../../shared/title.jsx";
+import {userContext} from "../../context/StudentContextProvider.jsx";
 
 
 export default function StudentTab3() {
+  const {getStudentTask} = useContext(userContext);
+  const [ tasks, setTasks] = useState([]);
+  useEffect(()=>{
+    const fetchData = async()=>{
+      const myTask = await getStudentTask();
+      setTasks(myTask.tasks);
+      console.log(tasks)
+    }
+    fetchData();
+  },[])
   return (
     <Box>
     <Title title={"Your Tasks"} />
     <Grid container>
       <Grid item xs={12} md={7} sx={{pb:2,pt:{xs:0,sm:1,md:7}}}>
-      <Task/>
-    
+      <Task tasks={tasks}/>
       </Grid>
       <Grid item xs={12} md={5}>
       <BasicDateCalendar style={{ border: "1px solid rgba(43, 1, 62, 0.4)" }} />
