85aede6d6e345f109555021b67d1fc156fb45ee6
Tasneem <tasneemgazal5@gmail.com>
2024-03-27 10:42:17 +0200

    update edit task content

--- src/components/Supervisor/supervisor_2/EditTask.jsx
+++ src/components/Supervisor/supervisor_2/EditTask.jsx
@@ -1,11 +1,199 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import AddTask from './AddTask.jsx'
+import React, { useContext, useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
 
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import InputCom from "../../shared/InputCom.jsx";
+import BasicDateTimePicker from "../../shared/BasicDateTimePicker.jsx";
+import UploadFile from "../../shared/UploadFile.jsx";
+import { useFormik } from "formik";
+import axios from "axios";
+import { SectionContext } from "../../context/SectionContextProvider.jsx";
+import SelectCom from "../../shared/SelectCom.jsx";
+import { useSnackbar } from "../../context/SnackbarProvider.jsx";
+import { useParams } from "react-router-dom";
+import { TaskContext } from "../../context/TaskContext.jsx";
+import { parse } from 'date-fns';
 export default function EditTask() {
+  const{id}=useParams();
+  const { getSuperSections } = useContext(SectionContext);
+  const { getTaskById } = useContext(TaskContext);
+  const token = localStorage.getItem("userToken");
+  const [sections, setSections] = useState([]);
+  const [selectedThesis, setSelectedThesis] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const theme = useTheme();
+  const { showSnackbar } = useSnackbar();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  async function fetchTaskData(taskId) {
+    const res = await getTaskById(taskId);
+    console.log(res);
+    const parsedStartDate = parse(res.tasks.startDate, 'MM/dd/yyyy hh:mm aa', new Date());
+    const parsedEndDate = parse(res.tasks.endDate, 'MM/dd/yyyy hh:mm aa', new Date());
+    setStartDate(parsedStartDate);
+    setEndDate(parsedEndDate);
+    formik.setValues({
+      txt: res.tasks.txt,
+      sections: res.tasks.sections,
+      task: res.tasks.file
+    });
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const sec = await getSuperSections();
+      setSections(sec);
+      console.log(sections);
+    };
+    fetchData();
+    fetchTaskData(id);
+  }, []);
+  const initialValues = {
+    txt: "",
+    file: "",
+    startDate: "",
+    endDate: "",
+  };
+
+  const onSubmit = async (values) => {
+    const formData = new FormData();
+    formData.append('txt', values.txt);
+    formData.append('task', selectedThesis);
+    formData.append('startDate', startDate);
+    formData.append('endDate', endDate);
+    sections.forEach((section, index) => {
+      if (values.sections.includes(section._id)) {
+        formData.append(`sections[${index}]`, section._id);
+      }
+    });
+  
+    try {
+      const { data } = await axios.patch(`${import.meta.env.VITE_API_URL}/supervisor/editTask/${id}`, formData, {
+        headers: { token: `Bearer ${token}` },
+      });
+      console.log(data)
+      if (data.message === 'success') {
+        showSnackbar({ message: 'task updated successfully', severity: 'success' });
+      }
+    } catch (error) {
+      console.error('Submission error:', error);
+    }
+  };
+  
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validateOnBlur: true,
+    validateOnChange: false,
+  });
+
+  const inputs = [
+    {
+      id: "txt",
+      type: "text",
+      name: "txt",
+      title: "Text",
+      value: formik.values.txt,
+    },
+  ];
+  const renderInputs = inputs.map((input, index) => (
+    <InputCom
+      type={input.type}
+      name={input.name}
+      id={input.id}
+      title={input.title}
+      value={input.value}
+      placeholder={input.title}
+      onChange={input.onChange || formik.handleChange}
+      onBlur={formik.handleBlur}
+      touched={formik.touched}
+    />
+  ));
+  const handleSectionChange = (event) => {
+    formik.setFieldValue("sections", event.target.value);
+  };
   return (
-   <Box>
-<AddTask title={"Edit the task"}/>
-   </Box>
-  )
+    <Container>
+      <Box>
+        <Typography
+          variant="h2"
+          sx={{
+            fontWeight: "bold",
+            pb: 2,
+            fontSize: { xs: "40px", md: "60px" },
+          }}
+        >
+          Edit Task
+        </Typography>
+      </Box>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={isSmallScreen ? 12 : 7}>
+          <form onSubmit={formik.handleSubmit} encType="multipart/form-data">
+            {renderInputs}
+            <BasicDateTimePicker label="From" onChange={setStartDate} value={startDate} />
+            <BasicDateTimePicker label="To" onChange={setEndDate} value={endDate}/>
+            <SelectCom
+              labelId="sections-label"
+              id="sections"
+              value={formik.values.sections || []} 
+              onChange={handleSectionChange}
+              label="sections"
+              options={sections.map((sec) => ({
+                value: sec._id,
+                label: sec.num,
+              }))}
+              multiple // Add the multiple prop here
+            />
+            <UploadFile
+              onFileChange={setSelectedThesis}
+              buttonText="Upload Thesis File"
+            />
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "rgba(43, 1, 62, 0.5)",
+                "&:hover": {
+                  backgroundColor: "rgba(43, 1, 62, 0.8)",
+                },
+                width: "100%",
+                mt: 1,
+              }}
+              type="submit"
+            >
+              Submit
+            </Button>
+          </form>
+        </Grid>
+
+        {!isSmallScreen && (
+          <Grid item xs={12} sm={5}>
+            <Box
+              sx={{
+                width: "90%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                pt: 3,
+              }}
+            >
+              <img
+                src="/image/addTask.png"
+                alt="Add Task"
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
+            </Box>
+          </Grid>
+        )}
+      </Grid>
+    </Container>
+  );
 }
+
