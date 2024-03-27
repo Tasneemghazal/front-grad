d87fc2f97cf96981c7c3fe85b04e6c9ad61ef14f
Tasneem <tasneemgazal5@gmail.com>
2024-03-27 10:41:54 +0200

    destruct taskId

--- src/components/Supervisor/supervisor_2/EditDeleteTask.jsx
+++ src/components/Supervisor/supervisor_2/EditDeleteTask.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import SpringModal from '../../shared/SpringModal.jsx';
 import ConfirmDelete from '../../shared/ConfirmDelete.jsx';
 
-export default function EditDeleteTask({ closeModal, onClickDelete }) {
+export default function EditDeleteTask({ closeModal, onClickDelete, taskId }) {
   const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
 
   const openConfirmationModal = () => {
@@ -26,7 +26,7 @@ export default function EditDeleteTask({ closeModal, onClickDelete }) {
       <Box sx={{ textAlign: "center" }}>
         <Typography sx={{ py: 2, fontWeight: "bold" }}>What do you want to do?</Typography>
         <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
-          <Link to='edittask'>
+          <Link to={`editTask/${taskId}`}> {/* Assuming you're using taskId for task ID */}
             <Button variant='contained' color="success" onClick={closeModal}>Edit</Button>
           </Link>
           <Button variant='contained' color="error" onClick={openConfirmationModal}>Delete</Button>
@@ -37,17 +37,8 @@ export default function EditDeleteTask({ closeModal, onClickDelete }) {
       <SpringModal
         isModalOpen={isConfirmationModalOpen}
         closeModal={closeConfirmationModal}
-        modalContent={<ConfirmDelete handleDeleteConfirm={handleDeleteConfirm} closeConfirmationModal={closeConfirmationModal} closeModal={closeModal}/>
-          // <Box>
-          //   <Typography variant="h6" sx={{ textAlign: 'center', mb: 2 }}>Are you sure you want to delete this task?</Typography>
-          //   <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          //     <Button variant="contained" color="error" onClick={handleDeleteConfirm}>Yes</Button>
-          //     <Button variant="contained" onClick={closeConfirmationModal}>No</Button>
-          //   </Box>
-          // </Box>
-        }
+        modalContent={<ConfirmDelete handleDeleteConfirm={handleDeleteConfirm} closeConfirmationModal={closeConfirmationModal} closeModal={closeModal} />}
       />
     </Container>
   );
 }
-
