bcda0ed6540eca39b077f3c65f3de2c8dc056c30
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-28 09:13:54 +0200

    Merge front page for technical support with back end point

--- src/components/mainPage/technicalSupport/TechnicalSupport.jsx
+++ src/components/mainPage/technicalSupport/TechnicalSupport.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import InputCom from "../../shared/InputCom";
-import { Box, Button, Container, FormGroup, Grid, Paper, Typography, useMediaQuery } from "@mui/material";
+import { Box, Button, Container, Grid, Paper, Typography, useMediaQuery } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { purple } from "@mui/material/colors";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useFormik } from "formik";
 
 const ColorButton = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(purple[500]),
@@ -15,7 +16,76 @@ const ColorButton = styled(Button)(({ theme }) => ({
 }));
 
 export default function TechnicalSupport() {
+  const initialValues = {
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  };
+
   const matches = useMediaQuery((theme) => theme.breakpoints.up('md'));
+
+  const onSubmit = async (values) => {
+    try {
+      await axios.post(`${import.meta.env.VITE_API_URL}/admin/technicalSupport`, values);
+      console.log('Message sent successfully!');
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validateOnBlur: true,
+    validateOnChange: false,
+  });
+
+  const inputs = [
+    {
+      id: "name",
+      type: "name",
+      name: "name",
+      title: "User Name",
+      value: formik.values.name,
+    },
+    {
+      id: "email",
+      type: "email",
+      name: "email",
+      title: "User Email",
+      value: formik.values.email,
+    },
+    {
+      id: "subject",
+      type: "text",
+      name: "subject",
+      title: "User Subject",
+      value: formik.values.subject,
+    },
+    {
+      id: "message",
+      type: "text",
+      name: "message",
+      title: "User Message",
+      value: formik.values.message,
+    },
+  ];
+
+  const renderInputs = inputs.map((input, index) => (
+    <InputCom
+      type={input.type}
+      name={input.name}
+      id={input.id}
+      title={input.title}
+      value={input.value}
+      key={index}
+      placeholder={input.name}
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      touched={formik.touched}
+    />
+  ));
 
   return (
     <Box
@@ -27,7 +97,6 @@ export default function TechnicalSupport() {
       }}
     >
       <Container maxWidth="lg">
-       
         <Grid container spacing={2} alignItems="center">
           <Grid item xs={12} md={6}>
             <Paper
@@ -38,20 +107,17 @@ export default function TechnicalSupport() {
                 p: 3
               }}
             >
-              <FormGroup sx={{ width: "100%" }}>
-              <Box sx={{textAlign:"center"}}>
-              <img src="image/ptuk.jpg" alt="ptuk logo"  width="30px" height="30px"/>
-              <Typography variant="p" sx={{display:"block", pb:1}}>Palestine Technical University</Typography>
-             <Typography variant="h4" sx={{ fontSize:"30px",textAlign: "center", mb: 4, fontWeight: "bold" }}>
-                Contact us
-              </Typography>
-             </Box>
-                <InputCom placeholder={"Your Name"} />
-                <InputCom placeholder={"Your Email"} />
-                <InputCom placeholder={"Subject"} />
-                <InputCom placeholder={"Your Message"} />
-                <ColorButton fullWidth>Send</ColorButton>
-              </FormGroup>
+              <form onSubmit={formik.handleSubmit} sx={{ width: "100%" }}>
+                <Box sx={{textAlign:"center"}}>
+                  <img src="image/ptuk.jpg" alt="ptuk logo"  width="30px" height="30px"/>
+                  <Typography variant="p" sx={{display:"block", pb:1}}>Palestine Technical University</Typography>
+                  <Typography variant="h4" sx={{ fontSize:"30px",textAlign: "center", mb: 4, fontWeight: "bold" }}>
+                    Contact us
+                  </Typography>
+                </Box>
+               {renderInputs}
+                <ColorButton fullWidth type="submit">Send</ColorButton>
+              </form>
             </Paper>
           </Grid>
           {matches && (
