dc9a3250c084f7b274fcd25b85ef6f28188b2674
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-29 13:20:31 +0300

    change the style for register page

--- src/components/student/Booking/SectionRegistration.jsx
+++ src/components/student/Booking/SectionRegistration.jsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Container, Typography, Box, Grid, Paper, Button } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Box,
+  Grid,
+  Paper,
+  Button,
+  Avatar,
+} from "@mui/material";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import SupervisorName from "./SupervisorName.jsx";
@@ -7,6 +15,7 @@ import SectionForm from "./SectionForm.jsx";
 import { SectionContext } from "../../context/SectionContextProvider.jsx";
 import EventIcon from "@mui/icons-material/Event";
 import Title from "../../shared/title.jsx";
+
 const SectionRegistration = () => {
   const token = localStorage.getItem("userToken");
   const [section, setSection] = useState([]);
@@ -25,9 +34,25 @@ const SectionRegistration = () => {
     fetchData();
   }, [getSections]);
 
+  const avatarImages = [
+    "/image/class.png",
+    "/image/class2.png",
+    "/image/class3.png",
+    "/image/class4.png",
+    "/image/class5.png",
+    "/image/class6.png",
+    "/image/class7.png",
+    "/image/class8.png",
+    "/image/class9.png",
+  ];
+
+  const getRandomAvatar = () => {
+    return avatarImages[Math.floor(Math.random() * avatarImages.length)];
+  };
+
   const handleSectionUpdate = (updatedSection) => {
-    setSection(prevSections =>
-      prevSections.map(sec =>
+    setSection((prevSections) =>
+      prevSections.map((sec) =>
         sec._id === updatedSection._id ? updatedSection : sec
       )
     );
@@ -36,49 +61,112 @@ const SectionRegistration = () => {
   return (
     <Container>
       <ToastContainer />
-   <Title title={"Book Your Supervisor"} />
-      <Grid container justifyContent="center" spacing={2}>
-        {section.map((sec) => (
-          sec.visible && (
-            <Grid key={sec._id} item xs={12} sm={6} md={4}>
-              <Paper
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  padding: 3,
-                  border: "1px solid rgba(43, 1, 62, 0.4)",
-                  transition: "box-shadow 0.3s, transform 0.3s",
-                  "&:hover": {
-                    transform: "translateY(-5px)",
-                    boxShadow: "0px 10px 15px rgba(0, 0, 0, 0.2)",
-                    cursor: "pointer",
-                  },
-                }}
-              >
-                <Box
+      <Typography
+        variant="h4"
+        sx={{
+          fontWeight: "bold",
+          color: "#4a148c",
+          textAlign: "center",
+          marginBottom: 4,
+          animation: "fadeIn 2s ease-in-out infinite",
+          "@keyframes fadeIn": {
+            "0%": {
+              opacity: 0,
+            },
+            "100%": {
+              opacity: 5,
+            },
+          },
+        }}
+      >
+        Registration instructions !
+      </Typography>
+      <Typography
+  variant="body1"
+  sx={{
+    fontSize: "1.1rem",
+    fontWeight: "normal",
+    lineHeight: 1.5,
+    mb: 10,
+    mx: 5,
+   
+  }}
+>
+  <ul>
+    <li>
+      All you have to do is choose the session number. 
+    </li>
+    <li>
+    Enter your number and
+      the numbers of your colleagues in the same group in addition to the
+      names.
+    </li>
+    <li>
+    Enter your number and
+      the numbers of your colleagues in the same group in addition to the
+      names.
+    </li>
+    <li>
+    You cannot withdraw the
+      request except after the supervisor approves or rejects it.
+    </li>
+    <li style={{ color: "#4a148c" }}>
+      (You can communicate via chat with the supervisor for any emergency).
+    </li>
+  </ul>
+</Typography>
+
+
+      <Title title={"Book Your Supervisor"} />
+      <Grid container justifyContent="center" textAlign="center" spacing={2}>
+        {section.map(
+          (sec) =>
+            sec.visible && (
+              <Grid key={sec._id} item xs={12} md={6}>
+                <Paper
                   sx={{
-                    borderRadius: "50%",
-                    width: "50px",
-                    height: "50px",
                     display: "flex",
-                    justifyContent: "center",
+                    flexDirection: "row",
                     alignItems: "center",
-                    backgroundColor: "#7f668b",
-                    marginBottom: 2,
+                    padding: 3,
+                    border: "1px solid rgba(43, 1, 62, 0.4)",
+                    transition: "box-shadow 0.3s, transform 0.3s",
+                    "&:hover": {
+                      transform: "translateY(-5px)",
+                      boxShadow: "0px 10px 15px rgba(0, 0, 0, 0.2)",
+                      cursor: "pointer",
+                    },
                   }}
                 >
-                  <EventIcon sx={{ color: "white" }} />
-                </Box>
-                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                  Section Number: {sec.num}
-                </Typography>
-                <SupervisorName userId={sec.userId} />
-                <SectionForm section={sec} token={token} onUpdateSection={handleSectionUpdate} />
-              </Paper>
-            </Grid>
-          )
-        ))}
+                  <Box sx={{ padding: 4, textAlign: "center" }}>
+                    <Avatar
+                      src={getRandomAvatar()}
+                      alt="Avatar"
+                      sx={{
+                        width: 100,
+                        height: 100,
+                        margin: "auto",
+                        border: "1px solid #000",
+                      }}
+                    />{" "}
+                  </Box>
+                  <Box sx={{ paddingLeft: 4 }}>
+                    {" "}
+                    {/* Add padding to separate avatar from content */}
+                    <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                      Section Number: {sec.num}
+                    </Typography>
+                    <SupervisorName userId={sec.userId} />
+                    <SectionForm
+                      section={sec}
+                      token={token}
+                      onUpdateSection={handleSectionUpdate}
+                    />
+                  </Box>
+                </Paper>
+              </Grid>
+            )
+        )}
       </Grid>
     </Container>
   );
