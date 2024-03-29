8e7146e9857bde6b0ff407aacf64783ce49f2df0
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-29 13:24:53 +0300

    change the icon for the supervisor tab4

--- src/components/Supervisor/supervisor_2/Supervisor_2.jsx
+++ src/components/Supervisor/supervisor_2/Supervisor_2.jsx
@@ -4,6 +4,7 @@ import DynamicTabs from '../../shared/DynamicTabs.jsx'
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 import ViewListIcon from '@mui/icons-material/ViewList';
 import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import FeedbackIcon from '@mui/icons-material/Feedback';
 import SupervisorTab1 from './SupervisorTab1.jsx';
 import SupervisorTab2 from './SupervisorTab2.jsx';
 import SupervisorTab3 from './SupervisorTab3.jsx';
@@ -16,7 +17,7 @@ export default function Supervisor_2() {
             { label: <ViewListIcon/>, content: <SupervisorTab1/> },
             { label: <BorderColorIcon/>, content: <SupervisorTab2/> },
             { label: <AppRegistrationIcon/>, content: <SupervisorTab3/> },
-            { label: <AppRegistrationIcon/>, content: <SupervisorTab4/> },
+            { label: <FeedbackIcon/>, content: <SupervisorTab4/> },
           ]}
         />
     </Container>
