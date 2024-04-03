20618ce7c49f8e2a7c65d7f2816f58964ff90067
Tasneem <tasneemgazal5@gmail.com>
2024-04-03 23:25:01 +0300

    pass removeSection as props to dynamicSection

--- src/components/HeadOD/Head2/HeadTab1.jsx
+++ src/components/HeadOD/Head2/HeadTab1.jsx
@@ -3,11 +3,11 @@ import { SectionContext } from "../../context/SectionContextProvider.jsx";
 import DynamicSection from "../../shared/DynamicSection.jsx";
 
 export default function HeadTab1() {
-  const { getSections } = useContext(SectionContext);
+  const { getSections,removeSection } = useContext(SectionContext);
 
   return (
     <>
-    <DynamicSection getSections={getSections}/>
+    <DynamicSection getSections={getSections} removeSection={removeSection} />
     </>
   );
 }
