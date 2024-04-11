97d3147b7d6d3b739d33def24e4c1d28ab03f5e6
Tasneem <tasneemgazal5@gmail.com>
2024-04-11 16:12:06 +0200

    create signInModalContent cmponent

---
+++ src/components/shared/SignModalContent.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+
+export default function SignModalContent({
+  userRoles,
+  handleNavigation,
+  setOpenModal,
+}) {
+  return (
+    <Box>
+      <Typography id="modal-modal-title" variant="h6" component="h2">
+        Choose Role
+      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+        {userRoles.map((role, index) => (
+          <Button
+            key={index}
+            onClick={() => {
+              handleNavigation(role);
+              setOpenModal(false);
+            }}
+            sx={{
+              color:"#fff",
+              backgroundColor: "rgba(43, 1, 62, 0.7)",
+              "&:hover": {
+                backgroundColor: "rgba(43, 1, 62, 0.8)",
+              },
+            }}
+          >
+            {role}
+          </Button>
+        ))}
+      </Box>
+    </Box>
+  );
+}
