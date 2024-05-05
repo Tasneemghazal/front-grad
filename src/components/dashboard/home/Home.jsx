6a160b46030b4ac266feccf9f7b9afb4019bab9c
FatimaOmair <fatimaemer2002@gmail.com>
2024-05-05 15:19:00 +0300

    edit the order of home page for the admin

--- src/components/dashboard/home/Home.jsx
+++ src/components/dashboard/home/Home.jsx
@@ -14,13 +14,13 @@ export default function Home() {
           <Chart1 />
         </Grid>
         <Grid component={Paper} item xs={12} sm={12} md={4} sx={{display:"flex", justifyContent:"center", alignItems:"center", mx:1,mt:1}}>
-        <Chart2 />
+        <Calender style={{width:"100%",height:"100%"}}/>
         </Grid>
         <Grid item component={Paper} xs={12} sm={12} md={6} sx={{display:"flex", justifyContent:"center", alignItems:"center", mx:1,mt:1,mb:{xs:2,lg:0} ,height:"310px"}}>
-        <BasicLineChart/>
+       <BasicLineChart/>
       </Grid>
       <Grid item component={Paper} xs={12} sm={12} md={5} sx={{display:"flex", justifyContent:"center", alignItems:"center", mx:1,mt:{xs:0,lg:1},mb:{xs:2,lg:0}, height:"310px",overflow:"hidden",pb:"30px"}}>
-        <Calender style={{width:"100%",height:"100%"}}/>
+        <Chart2/>
       </Grid>
       </Grid>
 
