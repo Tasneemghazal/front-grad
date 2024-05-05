6262840e36f32a75f569d7144cc6f64ff0770bc8
FatimaOmair <fatimaemer2002@gmail.com>
2024-05-05 15:20:02 +0300

    remove id

--- src/components/dashboard/student/Students.jsx
+++ src/components/dashboard/student/Students.jsx
@@ -32,7 +32,7 @@ export default function Students() {
         const res = await getUsers();
         if (res.users.length > 0) {
           const userKeys = Object.keys(res.users[0]);
-          const columns = ['_id', 'name', 'email'];
+          const columns = [ 'name', 'email','phoneNumber'];
           setTableColumns(columns);
           setTableData(res.users);
         }
