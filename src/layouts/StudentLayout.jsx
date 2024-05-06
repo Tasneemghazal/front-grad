d9efc19f3d98593ed05749e5d544e519abed9ad9
FatimaOmair <fatimaemer2002@gmail.com>
2024-05-06 16:28:12 +0300

    add loading in studentLayout

--- src/layouts/StudentLayout.jsx
+++ src/layouts/StudentLayout.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import Navbar from '../components/shared/Navbar.jsx'
 import { Outlet } from 'react-router-dom'
+import Loader from '../components/loader/Loader.jsx';
 
 export default function StudentLayout() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false); 
+    }, 2500); 
+    return () => clearTimeout(timeout);
+  }, []);
   return (
+    <>
+    {loading ? (
+      <Loader/>
+    ) : (
     <>
     <Navbar navItems={["Home", "Profile", "Technical Support"]}/>
     <Outlet/>
     </>
+     )}
+     </>
   )
 }
