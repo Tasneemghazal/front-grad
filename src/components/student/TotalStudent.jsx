336adf76bad2852b713d6d2fbe61f74eef27b063
FatimaOmair <fatimaemer2002@gmail.com>
2024-05-06 16:27:03 +0300

    remove loading from TotalStudent

--- src/components/student/TotalStudent.jsx
+++ src/components/student/TotalStudent.jsx
@@ -6,22 +6,22 @@ import Loader from "../loader/Loader.jsx";
 import { useEffect, useState } from "react";
 
 export default function TotalStudent() {
-  let[loading, setLoading] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000); 
+  // let[loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000); 
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
-  if (loading) {
-    return (
-      <>
-      <Loader/>
-      </>
-    )
-  }
+  // if (loading) {
+  //   return (
+  //     <>
+  //     <Loader/>
+  //     </>
+  //   )
+  // }
   return (
     <>
       <Student_1 />
