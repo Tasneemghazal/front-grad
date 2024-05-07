dc1592092db793cd3d38c9a8122374a771e5a205
FatimaOmair <fatimaemer2002@gmail.com>
2024-05-07 12:51:15 +0300

    create send code component

--- src/components/sendCode/SendCode.jsx
+++ src/components/sendCode/SendCode.jsx
@@ -1,21 +1,79 @@
+
 import React from "react";
 import InputCom from "../shared/InputCom";
 import { Box, Button, Container, FormGroup, Grid, Paper, Typography, useMediaQuery } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { purple } from "@mui/material/colors";
 import { Link } from "react-router-dom";
+import {  useNavigate } from 'react-router-dom'
+import axios from "axios";
+import { useFormik } from "formik";
+
 
 const ColorButton = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(purple[500]),
-  backgroundColor: " rgba(43, 1, 62, 0.7)",
-  marginTop: 10,
+  backgroundColor: "rgba(43, 1, 62, 0.7)",
+ 
   "&:hover": {
-    backgroundColor: " rgba(43, 1, 62, 0.8)",
+    backgroundColor:"rgba(43, 1, 62, 0.8)",
   },
 }));
 
 export default function SendCode() {
+
+  const navigate=useNavigate();
+  const initialValues = {
+    email: "",
+  };
+
   const matches = useMediaQuery((theme) => theme.breakpoints.up('md'));
+
+  const onSubmit = async (values) => {
+    try {
+      const {data}=await axios.post(`${import.meta.env.VITE_API_URL}/auth/sendCode`, values);
+      console.log('Message sent successfully!');
+
+      if(data.message=="success"){
+        navigate('/forgotPassword');
+      }
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validateOnBlur: true,
+    validateOnChange: false,
+  });
+
+  const inputs = [
+    {
+      id: "email",
+      type: "email",
+      name: "email",
+      title: "User Email",
+      value: formik.values.email,
+    },
+    
+    
+  ];
+
+  const renderInputs = inputs.map((input, index) => (
+    <InputCom
+      type={input.type}
+      name={input.name}
+      id={input.id}
+      title={input.title}
+      value={input.value}
+      key={index}
+      placeholder={input.name}
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      touched={formik.touched}
+    />
+  ));
 
   return (
     <Box
@@ -24,7 +82,6 @@ export default function SendCode() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        
       }}
     >
       <Container maxWidth="lg">
@@ -37,30 +94,31 @@ export default function SendCode() {
                 backgroundColor: "#fff",
                 p: 3
               }}
-            > 
-             <Box sx={{textAlign:"center"}}>
-              <img src="image/ptuk.jpg" alt="ptuk logo"  width="40px" height="40px"/>
-              <Typography variant="p" sx={{display:"block", pb:1}}>Palestine Technical University</Typography>
-             <Typography variant="h4" sx={{ fontSize:"30px",textAlign: "center", mb: 4, fontWeight: "bold" }}>
-                Send Code
-              </Typography>
-             </Box>
-              <FormGroup sx={{ width: "100%", mb: 3 }}>
-                <InputCom placeholder={"Email"} type={"email"} />
-               
-               
-                <ColorButton fullWidth>Send Code</ColorButton>
-              </FormGroup>
+            >
+              <form onSubmit={formik.handleSubmit} sx={{ width: "100%" }}>
+                <Box sx={{textAlign:"center"}}>
+                  <img src="image/ptuk.jpg" alt="ptuk logo"  width="30px" height="30px"/>
+                  <Typography variant="p" sx={{display:"block", pb:1}}>Palestine Technical University</Typography>
+                  <Typography variant="h4" sx={{ fontSize:"30px",textAlign: "center", mb: 4, fontWeight: "bold" }}>
+                  Send Code
+                  </Typography>
+                </Box>
+               {renderInputs}
+                <ColorButton fullWidth type="submit">Send</ColorButton>
+              </form>
             </Paper>
           </Grid>
           {matches && (
             <Grid item xs={12} md={6}>
-              <Box sx={{ display: "flex", justifyContent: "center" }}>
-                <img src="image/sendCode.gif" alt="" style={{ maxWidth: "100%", height: "auto" }} />
+              <Box sx={{ display: "flex", justifyContent: "center",position:"relative" }}>
+              <img src="image/sendCode.gif" alt="" style={{ maxWidth: "100%", height: "auto" }} />
+               
               </Box>
             </Grid>
           )}
+          
         </Grid>
+        
       </Container>
     </Box>
   );
