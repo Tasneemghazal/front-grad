9a965d6bf6b265b751b7df0c6e53b362ab5da6fb
Tasneem <tasneemgazal5@gmail.com>
2024-05-11 10:40:56 +0300

    create chat app component

--- src/components/ChatApp/ChatApp.jsx
+++ src/components/ChatApp/ChatApp.jsx
@@ -1,17 +1,71 @@
-import { Grid, Paper } from '@mui/material';
-import ChatSidebar from './ChatSidebar'; 
-import ChatMessages from './ChatMessages';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Box, Button } from '@mui/material';
 
-const ChatApp = () => {
+const ChatApp = ({ userId, userType }) => {
+  const token = localStorage.getItem("userToken");
+  const [chats, setChats] = useState([]);
+  const [activeChat, setActiveChat] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [socket, setSocket] = useState(null);
+
+
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/chat/fetchChat`, {
+          headers: { token: `Bearer ${token}` },
+        });
+        setChats(response.data);
+        console.log(response)
+      } catch (error) {
+        console.error('Error fetching chats:', error);
+      }
+    };
+    fetchChats();
+  }, []);
+
+  useEffect(()=>{
+    const fetchMessages = async (chatId) => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/chat/getmessages/6630af90982034e5cce06e03
+        `, {
+          headers: { token: `Bearer ${token}` },
+        });
+        console.log(response)
+        setMessages(response.data);
+      } catch (error) {
+        console.error('Error fetching messages:', error);
+      }
+    };
+    fetchMessages();
+  },[])
+
+ 
+
+  const sendMessage = async() => {
+    const message = {
+      content:"hello"
+    }
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/chat/sendmessage/6630af90982034e5cce06e03
+      `, message,{
+        headers: { token: `Bearer ${token}` },
+      });
+      console.log(response)
+    } catch (error) {
+      console.error('Error fetching messages:', error);
+    }
+  };
+
+
+  
+
+
   return (
-    <Grid container spacing={1} sx={{marginTop:"-60px"}}>
-      <Grid item xs={3}>
-        <ChatSidebar /> 
-      </Grid>
-      <Grid item xs={9}>
-        <Paper style={{ padding: '20px' }}><ChatMessages/></Paper>
-      </Grid>
-    </Grid>
+   <Button onClick={sendMessage}>app</Button>
   );
 };
 
