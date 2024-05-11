b6a0a0c7b48f9a153641e3e4dad948d7584786f0
FatimaOmair <fatimaemer2002@gmail.com>
2024-05-11 12:50:00 +0300

    add import chart 2 in home

--- src/components/dashboard/home/Home.jsx
+++ src/components/dashboard/home/Home.jsx
@@ -1,6 +1,7 @@
 import { Box, Grid, Paper } from '@mui/material'
 import React from 'react'
 import Chart1 from './Chart1'
+import Chart2 from './Chart2'
 import Calender from '../../shared/BasicDateCalender.jsx'
 import BasicLineChart from './BasicLineChart'
 import './home.css'
