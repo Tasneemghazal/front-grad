ad4373bf3eecb8032d5bf240a11f327558daaede
Tasneem <tasneemgazal5@gmail.com>
2024-05-31 18:13:59 +0300

    add css style

--- src/components/mainPage/mainPage_4/MainPage_4.jsx
+++ src/components/mainPage/mainPage_4/MainPage_4.jsx
@@ -17,7 +17,7 @@ export default function MainPage_4() {
    <Box sx={{ flexGrow: 1 }}>
   <Grid container spacing={2} sx={{mt:3}}>
     <Grid item xs={12} md={3}>
-      <Paper sx={{ padding: 3 ,'&:hover':{backgroundColor:'#e0e0e0', transition:'0.5s'}}} >
+      <Paper sx={{ padding: 3 ,'&:hover':{backgroundColor:'#e0e0e0', transition:'0.5s',scale:'1.1'}}} >
         <Typography variant="h5" sx={{fontSize:"20px",fontWeight:"900"}}>Sign In</Typography>
         <Typography>
         Go to the site, choose the admin, wait for approval, and then you can communicate with him
@@ -25,27 +25,27 @@ export default function MainPage_4() {
         </Typography>
       </Paper>
 
-      <Paper sx={{ padding: 3, marginTop: 2,'&:hover':{backgroundColor:'#e0e0e0', transition:'0.5s'} }}>
+      <Paper sx={{ padding: 3, marginTop: 2,'&:hover':{backgroundColor:'#e0e0e0', transition:'0.5s',scale:'1.1'} }}>
         <Typography variant="h5" sx={{fontSize:"20px",fontWeight:"900",pb:1}}>Get access</Typography>
         <Typography>Access to question bank of over 2000 questions</Typography>
       </Paper>
     </Grid>
 
     <Grid item xs={6} md={6} sx={{ margin: { xs: "auto", md: 0 }, width: "60%", display: { xs: "none", md: "flex" },flexDirection:{md:"column"}, justifyContent:{md:"center"} }}>
-      <Paper sx={{ margin: "auto", padding: 3,width:"60%" }}>
+      <Paper sx={{ margin: "auto", padding: 3,width:"60%",'&:hover':{backgroundColor:'#e0e0e0', transition:'0.5s',scale:'1.1'} }}>
         <img src="image/howitwork.jpg" alt="" style={{ width: "100%", margin: "auto", padding: 10 , boxShadow: "4px 4px 8px 5px rgba(0, 0, 0, 0.6)"}}/>
       </Paper>
     </Grid>
 
     <Grid item xs={12} md={3}>
-      <Paper sx={{ padding: 3 ,'&:hover':{backgroundColor:'#e0e0e0', transition:'0.5s'}}}>
+      <Paper sx={{ padding: 3 ,'&:hover':{backgroundColor:'#e0e0e0', transition:'0.5s',scale:'1.1'}}}>
         <Typography variant="h5" sx={{fontSize:"20px",fontWeight:"900"}}>Assignments</Typography>
         <Typography>
           You must submit your assignments on time and communicate with your supervisor when needed
         </Typography>
       </Paper>
 
-      <Paper sx={{ padding: 3, marginTop: 2,'&:hover':{backgroundColor:'#e0e0e0', transition:'0.5s'} }}>
+      <Paper sx={{ padding: 3, marginTop: 2,'&:hover':{backgroundColor:'#e0e0e0', transition:'0.5s',scale:'1.1'} }}>
         <Typography variant="h5" sx={{fontSize:"20px",fontWeight:"900"}}> feedback</Typography>
         <Typography>Your supervisor will mark and comment on your submission</Typography>
       </Paper>
